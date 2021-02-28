32070cf7de35829c1bca6a8c5730e0408bac9d7b
siddh <siddhant3646@gmail.com>
2021-02-28 13:02:28 +0530

    hi

--- app/src/main/java/org/tensorflow/lite/examples/classification/Complaint.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Complaint.java
@@ -2,13 +2,161 @@ package org.tensorflow.lite.examples.classification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Complaint extends AppCompatActivity {
+    Button btnbrowse, btnupload;
+    EditText txtdata ;
+    EditText txtdata2 ;
+    EditText txtdata3 ;
+    ImageView imgview;
+    Uri FilePathUri;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+    int Image_Request_Code = 7;
+    ProgressDialog progressDialog ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+        storageReference = FirebaseStorage.getInstance().getReference("Images");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Images");
+        btnbrowse = (Button)findViewById(R.id.btnbrowse);
+        btnupload= (Button)findViewById(R.id.btnupload);
+        txtdata = (EditText)findViewById(R.id.txtdata);
+        txtdata2 = (EditText)findViewById(R.id.txtdata2);
+        txtdata3 = (EditText)findViewById(R.id.txtdata3);
+        imgview = (ImageView)findViewById(R.id.image_view);
+        progressDialog = new ProgressDialog(Complaint.this);
+
+
+        btnbrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), Image_Request_Code);
+
+            }
+        });
+        btnupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UploadImage();
+
+            }
+        });
+        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Image_Request_Code && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            FilePathUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), FilePathUri);
+                imgview.setImageBitmap(bitmap);
+            }
+            catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public String GetFileExtension(Uri uri) {
+
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri)) ;
+
+    }
+
+
+    public void UploadImage() {
+
+        if (FilePathUri != null) {
+
+            progressDialog.setTitle("Image is Uploading...");
+            progressDialog.show();
+            StorageReference storageReference2 = storageReference.child(System.currentTimeMillis() + "." + GetFileExtension(FilePathUri));
+            storageReference2.putFile(FilePathUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            String TempImageName = txtdata.getText().toString().trim();
+                            String TempImageName2 = txtdata2.getText().toString().trim();
+                            String TempImageName3 = txtdata3.getText().toString().trim();
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Image Uploaded Successfully ", Toast.LENGTH_LONG).show();
+                            Intent i=new Intent(Complaint.this, CameraActivity.class);
+                            startActivity(i);
+                            finish();
+                            @SuppressWarnings("VisibleForTests")
+                            uploadinfo imageUploadInfo = new uploadinfo(TempImageName,TempImageName2,TempImageName3, taskSnapshot.getUploadSessionUri().toString());
+                            String ImageUploadId = databaseReference.push().getKey();
+                            databaseReference.child(ImageUploadId).setValue(imageUploadInfo);
+                        }
+                    });
+        }
+        else {
+
+            Toast.makeText(Complaint.this, "Please Select Image or Add Image Name", Toast.LENGTH_LONG).show();
+
+        }
+    }
+    public class uploadinfo {
+
+        public String imageName;
+        public String imageEmail;
+        public String imageAddress;
+        public String imageURL;
+        public uploadinfo(){}
+
+        public uploadinfo(String name,String email ,String  address, String url) {
+            this.imageName = name;
+            this.imageAddress=address;
+            this.imageEmail=email;
+            this.imageURL = url;
+        }
+
+        public String getImageName() {
+            return imageName;
+        }
+        public String getImageURL() {
+            return imageURL;
+        }
     }
 }
